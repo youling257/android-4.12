a301517c587eecea998da5a6da76fb8ac8fd5703
Jens Kuske <jenskuske@gmail.com>
2015-05-15 18:38:53 +0200

    clk: sunxi: Let divs clocks read the base factor clock name from devicetree
    Currently, the sunxi clock driver gets the name for the base factor clock
    of divs clocks from the name field in factors_data. This prevents reusing
    of the factor clock for clocks with same properties, but different name.
    
    This commit makes the divs setup function try to get a name from
    clock-output-names in the devicetree. It also removes the name field where
    possible and merges the sun4i PLL5 and PLL6 clocks.
    
    The sun4i PLL5 clock doesn't have a output for the base factor clock,
    so we still have to use the name field there.
    
    Signed-off-by: Jens Kuske <jenskuske@gmail.com>

--- drivers/clk/sunxi/clk-sunxi.c
+++ drivers/clk/sunxi/clk-sunxi.c
@@ -707,18 +707,10 @@ static const struct factors_data sun4i_pll5_data __initconst = {
 	.name = "pll5",
 };
 
-static const struct factors_data sun4i_pll6_data __initconst = {
-	.enable = 31,
-	.table = &sun4i_pll5_config,
-	.getter = sun4i_get_pll5_factors,
-	.name = "pll6",
-};
-
 static const struct factors_data sun6i_a31_pll6_data __initconst = {
 	.enable = 31,
 	.table = &sun6i_a31_pll6_config,
 	.getter = sun6i_a31_get_pll6_factors,
-	.name = "pll6x2",
 };
 
 static const struct factors_data sun5i_a13_ahb_data __initconst = {
@@ -941,7 +933,7 @@ static const struct divs_data pll5_divs_data __initconst = {
 };
 
 static const struct divs_data pll6_divs_data __initconst = {
-	.factors = &sun4i_pll6_data,
+	.factors = &sun4i_pll5_data,
 	.ndivs = 4,
 	.div = {
 		{ .shift = 0, .table = pll6_sata_tbl, .gate = 14 }, /* M, SATA */
@@ -983,6 +975,7 @@ static void __init sunxi_divs_clk_setup(struct device_node *node,
 	struct clk_gate *gate = NULL;
 	struct clk_fixed_factor *fix_factor;
 	struct clk_divider *divider;
+	struct factors_data factors = *data->factors;
 	void __iomem *reg;
 	int ndivs = SUNXI_DIVS_MAX_QTY, i = 0;
 	int flags, clkflags;
@@ -991,8 +984,17 @@ static void __init sunxi_divs_clk_setup(struct device_node *node,
 	if (data->ndivs)
 		ndivs = data->ndivs;
 
+	/* Try to find a name for base factor clock */
+	for (i = 0; i < ndivs; i++) {
+		if (data->div[i].self) {
+			of_property_read_string_index(node, "clock-output-names",
+						      i, &factors.name);
+			break;
+		}
+	}
+
 	/* Set up factor clock that we will be dividing */
-	pclk = sunxi_factors_clk_setup(node, data->factors);
+	pclk = sunxi_factors_clk_setup(node, &factors);
 	parent = __clk_get_name(pclk);
 
 	reg = of_iomap(node, 0);
